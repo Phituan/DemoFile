dc99148b7413e8ebe6916c0333c247cbef8f3e7f
ok <tuandpd00617@fpt.edu.vn>
2018-10-11 13:35:11 +0700

    ok

---
+++ App6/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using App6.NewFolder1;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App6
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Customer currentCustomer;
+        public MainPage()
+        {
+            this.currentCustomer = new Customer();
+            this.InitializeComponent();
+        }
+
+        private async void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            currentCustomer.name = Name.Text;
+            currentCustomer.email = Email.Text;
+            currentCustomer.phone = Phone.Text;
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>(){".txt"});
+            savePicker.SuggestedFileName = "New Document";
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            string json = JsonConvert.SerializeObject(currentCustomer);
+            await FileIO.WriteTextAsync(file, json);
+            
+
+        
+    }
+
+        private async void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            openPicker.FileTypeFilter.Add(".txt");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            string content = await FileIO.ReadTextAsync(file);
+            Customer customer = JsonConvert.DeserializeObject<Customer>(content);
+
+            this.Name.Text = customer.name;
+            this.Email.Text = customer.email;
+            this.Phone.Text = customer.phone;
+        }
+    }
---
+++ App6/NewFolder1/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6.NewFolder1
+{
+    class Customer
+    {
+        private string _name;
+        private string _email;
+        private string _phone;
+
+        public string name { get => _name; set => _name = value; }
+        public string email { get => _email; set => _email = value; }
+        public string phone { get => _phone; set => _phone = value; }
+
+    }
+}
